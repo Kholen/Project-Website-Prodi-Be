66bc980177caa5718ecb7197ed81f4d4bd817fc1
Kholen <kholenlim28@gmail.com>
2025-09-29 16:44:12 +0700

    menghapus yang tidak dipakai

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -30,27 +30,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 /**
- * TEMPORARY ROUTE: Update User Password
- * REMOVE IMMEDIATELY AFTER USE!
+ * route "/kerjasama"
  * @method "GET"
- * @url /api/update-password?username=your_username&new_password=your_new_password
  */
-Route::get('/update-password', function (Request $request) {
-    $username = $request->query('username');
-    $newPassword = $request->query('new_password');
-
-    if (!$username || !$newPassword) {
-        return response()->json(['message' => 'Username and new_password are required.'], 400);
-    }
-
-    $user = App\Models\User::where('username', $username)->first();
-
-    if (!$user) {
-        return response()->json(['message' => 'User not found.'], 404);
-    }
-
-    $user->password = bcrypt($newPassword);
-    $user->save();
-
-    return response()->json(['message' => 'Password updated successfully.']);
-});
+Route::get('/kerjasama', App\Http\Controllers\Api\KerjasamaProdiController::class . '@index')->name('kerjasama.index');
