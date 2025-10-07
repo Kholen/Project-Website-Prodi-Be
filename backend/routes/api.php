b68457ff0120c67b4271d5b7f230c54d5c2d5253
YuriFerdinand <muhammadyuri965@gmail.com>
2025-10-07 12:06:33 +0700

    menambah route crud dosen

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -21,6 +21,21 @@ Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('lo
  */
 Route::get('/dosen', App\Http\Controllers\Api\DosenController::class . '@index')->name('dosen.index');
 
+Route::get('/dosen/{id}', App\Http\Controllers\Api\DosenController::class . '@show')->name('dosen.index');
+
+Route::put('/dosen/{id}', App\Http\Controllers\Api\DosenController::class . '@update')->name('dosen.update');
+
+Route::delete('/dosen/{id}', App\Http\Controllers\Api\DosenController::class . '@destroy')->name('dosen.destroy');
+
+Route::get('/jabatans', App\Http\Controllers\Api\JabatanController::class . '@index')->name('jabatans.index');
+
+Route::get('/skills', App\Http\Controllers\Api\SkillController::class . '@index')->name('skills.index');
+
+Route::post('/dosen', App\Http\Controllers\Api\DosenController::class . '@store')->name('dosen.store');
+
+Route::get('/prodi', App\Http\Controllers\Api\ProdiController::class . '@index')->name('dosen.store');
+
+
 /**
  * route "/user"
  * @method "GET"
