d8af05b7ef36419305fd4c53faa45cd14e543446
Kholen <kholenlim28@gmail.com>
2025-10-13 09:44:15 +0700

    create berita tggl 10

---
+++ backend/database/migrations/2025_10_10_030103_create_berita_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('berita', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->string('gambar_berita');
+            $table->text('kepala_berita');
+            $table->text('tubuh_berita');
+            $table->text('ekor_berita');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('berita');
+    }
+};
